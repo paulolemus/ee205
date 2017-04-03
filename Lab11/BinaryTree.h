89792c7b9f40a436381c4d5e71e364a799bdc0d8
Paulo L <plemus@hawaii.edu>
2017-04-02 23:36:15 -1000

    Made small additions to BinaryTree

--- Lab11/BinaryTree.h
+++ Lab11/BinaryTree.h
@@ -6,10 +6,41 @@
 #ifndef EE_LAB11_BINARYTREE_H_
 #define EE_LAB11_BINARYTREE_H_
 
+template <typename T>
+struct Node {
+    T data;
+    Node<T>* left;
+    Node<T>* right;
 
-class BinaryTree {
-
-
+    Node(const T& data);
 };
+
+template <typename T>
+class BinaryTree {
+    
+    Node<T>* root;
+
+public:
+    BinaryTree();
+    void insert(const T& data);
+};
+
+template <typename T>
+Node<T>::Node(const T& data) : 
+    data(data), 
+    left(nullptr), 
+    right(nullptr) {}
+
+template <typename T>
+BinaryTree<T>::BinaryTree() : root(nullptr) {}
+template <typename T>
+void BinaryTree<T>::insert(const T& data) {
+    if(root == nullptr) {
+        root = new Node<T>(data);
+    }
+    else {
+        // TODO: Finish this
+    }
+}   
 
 #endif
