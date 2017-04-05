cf0c0be3addd758ca2c3dbc0c474297b4e7a71a8
Paulo L <plemus@hawaii.edu>
2017-04-04 22:55:59 -1000

    Began to fix binary tree, but got stuck. Need to figure out how to keep the complete binary tree on each side of the root using linked nodes

--- Lab11/BinaryTree.h
+++ Lab11/BinaryTree.h
@@ -6,6 +6,12 @@
 #ifndef EE_LAB11_BINARYTREE_H_
 #define EE_LAB11_BINARYTREE_H_
 
+
+
+////////////////////////
+//    DECLARATIONS    //
+////////////////////////
+
 template <typename T>
 struct Node {
     T data;
@@ -17,13 +23,23 @@ struct Node {
 
 template <typename T>
 class BinaryTree {
-    
+
     Node<T>* root;
+
+
+    // private recursive assistance functions
+    void insert_r_assist(const T& data, Node<T>*& ptrptr);
 
 public:
     BinaryTree();
-    void insert(const T& data);
+
+    void insert  (const T& data);
+    void insert_r(const T& data);
 };
+
+////////////////////////
+//   IMPLEMENTATION   //
+////////////////////////
 
 template <typename T>
 Node<T>::Node(const T& data) : 
@@ -33,14 +49,83 @@ Node<T>::Node(const T& data) :
 
 template <typename T>
 BinaryTree<T>::BinaryTree() : root(nullptr) {}
+
+// Iterative implementation of insert
 template <typename T>
 void BinaryTree<T>::insert(const T& data) {
+    
+    // Base case, no root
     if(root == nullptr) {
         root = new Node<T>(data);
     }
+
+    // If the data is in root, do nothing
+    else if(root->data == data);
+
+    // otherwise traverse tree to insert
     else {
-        // TODO: Finish this
-    }
-}   
+
+        // Decide which side of tree data should be on
+        bool inserted = false;
+        Node<T>* ptr  = root;
+        if(data > ptr->data) ptr = ptr->right;
+        else                 ptr = ptr->left;
+
+        // Do a check to see if current node is null
+        if(ptr == nullptr) {
+            ptr = new Node<T>(data);
+            inserted = true;
+        }
+
+        while(!inserted) {
+
+            // place left if possible
+            if(ptr->left == nullptr) {
+                ptr->left = new Node<T>(data);
+                inserted = true;
+            }
+            // otherwise, place right if possible
+            else if(ptr->right == nullptr) {
+                ptr->right = new Node<T>(data);
+                inserted = true;
+            }
+            // Otherwise, move to child node with an open slot
+            else {
+                if(!ptr->left->left || !ptr->left->right)
+                    ptr = ptr->left;
+                else if(!ptr->right->left || !ptr->right->right)
+                    ptr = ptr->right;
+                else ptr = ptr->left;
+            }
+
+        } // while
+    } // else
+} // insert
+
+// Recursive implementation of insert
+template <typename T>
+void BinaryTree<T>::insert_r(const T& data) {
+
+    if(root == nullptr) root = new Node<T>(data);
+    else if(data == root->data);
+    else if(data > root->data) insert_r_assist(data, root->right);
+    else                       insert_r_assist(data, root->left);
+}
+template <typename T>
+void BinaryTree<T>::insert_r_assist(const T& data, Node<T>*& root) {
+
+    // If we have reached a nullptr, we have not found the data
+    // so add a new node with the data
+    if(root == nullptr) root = new Node<T>(data);
+
+    // Do nothing if the data exists in tree
+    else if(data == root->data);
+
+    // Move to right node if data is greater that current node data
+    else if(data > root->data) insert_r_assist(data, root->right);
+
+    // Otherwise move to the left
+    else insert_r_assist(data, root->left);
+}
 
 #endif
--- Lab11/tests/test-BinaryTree.cpp
+++ Lab11/tests/test-BinaryTree.cpp
@@ -3,6 +3,58 @@
 
 TEST(BinaryTree, constuctor) {
 
+    BinaryTree<int> tree;
+    ASSERT_TRUE(true);
+}
+
+TEST(BinaryTree, insert_base_ends) {
+
+    BinaryTree<int> tree;
+    tree.insert(1);
+    ASSERT_TRUE(true);
+}
+
+TEST(BinaryTree, insert_multiple_ends) {
+
+    BinaryTree<int> tree;
+    tree.insert(10);
+    ASSERT_TRUE(true);
+    tree.insert(11);
+    ASSERT_TRUE(true);
+    tree.insert(9);
+    ASSERT_TRUE(true);
+    tree.insert(8);
+    ASSERT_TRUE(true);
+    tree.insert(3);
+    ASSERT_TRUE(true);
+    tree.insert(5);
+    ASSERT_TRUE(true);
+    tree.insert(30);
+    ASSERT_TRUE(true);
+}
+
+TEST(BinaryTree, insert_r_base_ends) {
+    BinaryTree<int> tree;
+    tree.insert_r(1);
+    ASSERT_TRUE(true);
+}
+
+TEST(BinaryTree, insert_r_multiple_ends) {
+
+    BinaryTree<int> tree;
+    tree.insert_r(10);
+    ASSERT_TRUE(true);
+    tree.insert_r(11);
+    ASSERT_TRUE(true);
+    tree.insert_r(9);
+    ASSERT_TRUE(true);
+    tree.insert_r(8);
+    ASSERT_TRUE(true);
+    tree.insert_r(3);
+    ASSERT_TRUE(true);
+    tree.insert_r(5);
+    ASSERT_TRUE(true);
+    tree.insert_r(30);
     ASSERT_TRUE(true);
 }
 
